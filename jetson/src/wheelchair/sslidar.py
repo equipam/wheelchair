7884fa0edc25f39a5ef3462da0822770064fd6c2
DavidFreire-FEUP <up201806711@fe.up.pt>
2022-10-01 11:45:22 +0100

    Create python main routine

--- jetson/src/wheelchair/sslidar.py
+++ jetson/src/wheelchair/sslidar.py
@@ -1,3 +1,16 @@
+from wheelchair.sslidar_driver import SSLidarDriver
+
 class SSLidar:
-    def __init__(self, driver) -> None:
+    def __init__(self, driver: SSLidarDriver, threshold: float) -> None:
+        self._driver = driver
+        self._collision_threshold = threshold
         pass
+
+    @property
+    def distance(self):
+        # TODO Create function that gets the distance from Lidar
+        pass
+
+    @property
+    def collision(self):
+        return self.distance >= self._collision_threshold
