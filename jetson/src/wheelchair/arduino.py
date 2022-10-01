720592e430fe7b1dbd51e5d07c09f26f0b70e9b5
DavidFreire-FEUP <up201806711@fe.up.pt>
2022-10-01 11:26:02 +0100

    save state of arduino pin

--- jetson/src/wheelchair/arduino.py
+++ jetson/src/wheelchair/arduino.py
@@ -5,24 +5,22 @@ import Jetson.GPIO as GPIO       #Necessary inicial set up
 class Arduino:
     def __init__(self, pin_number) -> None:
         # setup pin
-        self.output_pin = pin_number  #Pin 18 advised (SPI Communication) -- board Pin number 12
+        self._output_pin = pin_number  #Pin 18 advised (SPI Communication) -- board Pin number 12
+        self.state = False
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.output_pin, GPIO.OUT, initial=GPIO.LOW)
-
-        pass
+        GPIO.setup(self._output_pin, GPIO.OUT, initial=GPIO.LOW)
 
     def set_stop_signal(self, state: bool):        
         # interface with pin
         if state == True:
             try:
-                GPIO.output(self.output_pin, GPIO.HIGH)
+                GPIO.output(self._output_pin, GPIO.HIGH)
+                self.state = True
             finally:
                 GPIO.cleanup()
-                pass
         else:
             try:
-                GPIO.output(self.output_pin, GPIO.LOW)
+                GPIO.output(self._output_pin, GPIO.LOW)
+                self.state = False
             finally:
                 GPIO.cleanup()
-                pass
-            pass
