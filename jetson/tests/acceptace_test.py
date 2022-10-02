290499b1345a80ba2346a0bd91e0a369b8c69904
DavidFreire-FEUP <up201806711@fe.up.pt>
2022-10-02 14:11:25 +0100

    add jetson nano acceptance tests

--- jetson/tests/acceptace_test.py
+++ jetson/tests/acceptace_test.py
@@ -1,11 +1,33 @@
-#TODO #5 Create jetson nano application acceptance test
-
 from unittest import mock
 from typer.testing import CliRunner
 from wheelchair.main import app
 
-@mock.patch("src.main.SSLidarDriver")
-def test_acceptance(mock_driver):
+
+@mock.patch("wheelchair.main.SSLidar")
+@mock.patch("wheelchair.main.Arduino")
+def test_collision(mock_arduino, mock_lidar):
     runner = CliRunner()
-    result = runner.invoke(app, ["--once"])
+
+    mock_lidar.return_value.configure_mock(collision=True)
+    mock_arduino.return_value.configure_mock(state=False)
+
+    result = runner.invoke(app, ["--once", "--verbose"])
+
+    mock_arduino.return_value.set_stop_signal.assert_called_with(True)
     assert result.exit_code == 0
+    assert "Collision!" in result.output
+
+
+@mock.patch("wheelchair.main.SSLidar")
+@mock.patch("wheelchair.main.Arduino")
+def test_no_collision(mock_arduino, mock_lidar):
+    runner = CliRunner()
+
+    mock_lidar.return_value.configure_mock(collision=False)
+    mock_arduino.return_value.configure_mock(state=True)
+
+    result = runner.invoke(app, ["--once", "--verbose"])
+
+    mock_arduino.return_value.set_stop_signal.assert_called_with(False)
+    assert result.exit_code == 0
+    assert "Not colliding anymore!" in result.output
