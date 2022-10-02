1958b47d42825c4c72f0687de9c72125338da670
up201806652 <78861546+up201806652@users.noreply.github.com>
2022-10-02 11:10:14 +0100

    Normalização das coordenadas do joystick
    Alterei funções joystickmapping e position do Joystick.cpp

--- arduino/main/Joystick.cpp
+++ arduino/main/Joystick.cpp
@@ -32,6 +32,8 @@ LinearCoords Joystick::joystickMapping(LinearCoords realCoords){
     // vx and vy are real values from the joystick
     // returns normalized values of x and y on a tuple
     LinearCoords normalizedCoords = {.x = 0.0, .y = 0.0};
+    normalizedCoords.x = realCoords.x / abs(realBounds.maxX-realBounds.minX);
+    normalizedCoords.y = realCoords.y / abs(realBounds.maxY-realBounds.minY); 
     return normalizedCoords;
 }
 
@@ -39,5 +41,7 @@ LinearCoords Joystick::position(){
     // magic ...
     //TODO #11 Create function that returns joystick normalized position
     LinearCoords pos = {.x = 0.0, .y = 0.0};
+    //get pos from joystick and normalize it
+    pos = joystickMapping(pos);
     return pos;
 }
