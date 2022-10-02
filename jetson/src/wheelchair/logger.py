415742c80b420915f877aa8d4ee31a88bf50ceda
DavidFreire-FEUP <up201806711@fe.up.pt>
2022-10-02 14:10:42 +0100

    print when testing

--- jetson/src/wheelchair/logger.py
+++ jetson/src/wheelchair/logger.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 class Logger:
     def __init__(self, level: str) -> None:
@@ -19,16 +20,26 @@ class Logger:
         logging.basicConfig(format='%(asctime)s - %(message)s', level=logging_level)
 
     def debug(self, message):
+        if os.getenv("TESTING") == "TRUE":
+            print(message)
         logging.debug(message)
 
     def info(self, message):
+        if os.getenv("TESTING") == "TRUE":
+            print(message)
         logging.info(message)
 
     def warning(self, message):
+        if os.getenv("TESTING") == "TRUE":
+            print(message)
         logging.warning(message)
 
     def error(self, message):
+        if os.getenv("TESTING") == "TRUE":
+            print(message)
         logging.error(message)
 
     def critical(self, message):
+        if os.getenv("TESTING") == "TRUE":
+            print(message)
         logging.critical(message)
