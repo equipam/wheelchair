20d587a46a1c8e4d2f7985bfcb2ae71d852731c8
DavidFreire-FEUP <up201806711@fe.up.pt>
2022-10-02 17:39:39 +0100

    use real analog values on real pin interface

--- arduino/main/PinInterface.h
+++ arduino/main/PinInterface.h
@@ -3,20 +3,25 @@
 
 #include <stdint.h>
 
-class PinInterface {
+#define FROM_ANALOG_COEFFICIENT 5 / 1023
+#define TO_ANALOG_COEFFICIENT 1023 / 5
+
+class PinInterface
+{
 public:
     virtual void doDigitalWrite(uint8_t pin, uint8_t val);
     virtual int doDigitalRead(uint8_t pin);
-    virtual void doAnalogWrite(uint8_t pin, int val);
-    virtual int doAnalogRead(uint8_t pin);
+    virtual void doAnalogWrite(uint8_t pin, float val);
+    virtual float doAnalogRead(uint8_t pin);
 };
 
-class RealPinInterface: public PinInterface{
+class RealPinInterface : public PinInterface
+{
 public:
     void doDigitalWrite(uint8_t pin, uint8_t val);
     int doDigitalRead(uint8_t pin);
-    void doAnalogWrite(uint8_t pin, int val);
-    int doAnalogRead(uint8_t pin);
+    void doAnalogWrite(uint8_t pin, float val);
+    float doAnalogRead(uint8_t pin);
 };
 
 #endif
--- arduino/main/RealPinInterface.cpp
+++ arduino/main/RealPinInterface.cpp
@@ -1,16 +1,23 @@
 #include "Arduino.h"
 #include "PinInterface.h"
 
-
-void RealPinInterface::doDigitalWrite(uint8_t pin, uint8_t val) {
+void RealPinInterface::doDigitalWrite(uint8_t pin, uint8_t val)
+{
     digitalWrite(pin, val);
 }
-int RealPinInterface::doDigitalRead(uint8_t pin) {
+
+int RealPinInterface::doDigitalRead(uint8_t pin)
+{
     return digitalRead(pin);
 }
-void RealPinInterface::doAnalogWrite(uint8_t pin, int val) {
+
+void RealPinInterface::doAnalogWrite(uint8_t pin, float val)
+{
+    int realVal = static_cast<int>(val * TO_ANALOG_COEFFICIENT);
     analogWrite(pin, val);
 }
-int RealPinInterface::doAnalogRead(uint8_t pin) {
-    return analogRead(pin);
+
+float RealPinInterface::doAnalogRead(uint8_t pin)
+{
+    return static_cast<float>(analogRead(pin) * FROM_ANALOG_COEFFICIENT);
 }
