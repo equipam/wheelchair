d442f6c912a4aeb68164f4371804cd8f42f1d983
up201806652 <78861546+up201806652@users.noreply.github.com>
2022-10-02 11:14:15 +0100

    Include
    Faltava o include para usar o abs()

--- arduino/main/Joystick.cpp
+++ arduino/main/Joystick.cpp
@@ -1,6 +1,7 @@
 //TODO #3 Identify axis boundaries (XY) on the joystick setup and map them to known scales
 
 #include "Joystick.h"
+#include <cstdlib>
 
 Joystick::Joystick(int xPin, int yPin, PinInterface* pinInterface){
     this->xPin = xPin;
