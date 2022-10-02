d7f96c4ed8016ad99fcc14a1d44b6004694513b9
DavidFreire-FEUP <up201806711@fe.up.pt>
2022-10-02 17:39:55 +0100

    use analog read instead of digital read for joystick position

--- arduino/main/Joystick.cpp
+++ arduino/main/Joystick.cpp
@@ -1,44 +1,50 @@
 #include "Joystick.h"
 
-Joystick::Joystick(int xPin, int yPin, PinInterface* pinInterface){
+Joystick::Joystick(int xPin, int yPin, PinInterface *pinInterface)
+{
     this->xPin = xPin;
     this->yPin = yPin;
     this->pinInterface = pinInterface;
     return;
 }
 
-void Joystick::calibrate(Stream &io){
-    //TODO #10 Create joystick calibration function
-    // ask the user to place the joystick in forward, back, left and write positions,
-    // storing the real bounds on the class attribute "realBounds"
+void Joystick::calibrate(Stream &io)
+{
+    // TODO #10 Create joystick calibration function
+    //  ask the user to place the joystick in forward, back, left and write positions,
+    //  storing the real bounds on the class attribute "realBounds"
 }
 
-void Joystick::calibrate_left(Stream &io){
+void Joystick::calibrate_left(Stream &io)
+{
     return;
 }
-void Joystick::calibrate_right(Stream &io){
+void Joystick::calibrate_right(Stream &io)
+{
     return;
 }
-void Joystick::calibrate_front(Stream &io){
+void Joystick::calibrate_front(Stream &io)
+{
     return;
 }
-void Joystick::calibrate_back(Stream &io){
+void Joystick::calibrate_back(Stream &io)
+{
     return;
 }
 
-LinearCoords Joystick::joystickMapping(LinearCoords realCoords){
-    LinearCoords normalizedCoords = {.x = 0.0, .y = 0.0};
-    normalizedCoords.x = realCoords.x / abs(realBounds.maxX-realBounds.minX);
-    normalizedCoords.y = realCoords.y / abs(realBounds.maxY-realBounds.minY); 
+LinearCoords Joystick::joystickMapping(LinearCoords realCoords)
+{
+    LinearCoords normalizedCoords;
+    normalizedCoords.x = realCoords.x / abs(realBounds.maxX - realBounds.minX);
+    normalizedCoords.y = realCoords.y / abs(realBounds.maxY - realBounds.minY);
     return normalizedCoords;
 }
 
-LinearCoords Joystick::position(){
-    // magic ...
-    //TODO #11 Create function that returns joystick normalized position
-    LinearCoords pos = {.x = 0.0, .y = 0.0};
-    pos.x = digitalRead(xPin);
-    pos.y = digitalRead(yPin);
+LinearCoords Joystick::position()
+{
+    LinearCoords pos;
+    pos.x = this->pinInterface->doAnalogRead(this->xPin);
+    pos.y = this->pinInterface->doAnalogRead(this->xPin);
     pos = joystickMapping(pos);
     return pos;
 }
