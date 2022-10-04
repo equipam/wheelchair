cb503b11adec1895d04db5c8bf5dda0b1a90df69
DavidFreire-FEUP <up201806711@fe.up.pt>
2022-10-04 11:13:55 +0100

    use float version of map in chair command

--- arduino/main/Chair.cpp
+++ arduino/main/Chair.cpp
@@ -8,9 +8,11 @@ Chair::Chair(int vPin, int omegaPin, PinInterface *pinInterface)
     return;
 }
 
-void Chair::command(PolarCoords coords)   //Chair command function
+/// @brief Sends a V and Omega command to the chair DB9
+/// @param coords Normalized polar coordinates
+void Chair::command(PolarCoords coords) // Chair command function
 {
-    this->pinInterface->doAnalogWrite(this->vPin, map(coords.v,STANDARDIZED_V_MIN,STANDARDIZED_V_MAX,OUTPUT_V_MIN,OUTPUT_V_MAX));
-    this->pinInterface->doAnalogWrite(this->omegaPin, map(coords.omega,STANDARDIZED_OMEGA_MIN,STANDARDIZED_OMEGA_MAX,OUTPUT_OMEGA_MIN,OUTPUT_OMEGA_MAX));
+    this->pinInterface->doAnalogWrite(this->vPin, Coordinates::map(coords.v, STANDARDIZED_V_MIN, STANDARDIZED_V_MAX, OUTPUT_V_MIN, OUTPUT_V_MAX));
+    this->pinInterface->doAnalogWrite(this->omegaPin, Coordinates::map(coords.omega, STANDARDIZED_OMEGA_MIN, STANDARDIZED_OMEGA_MAX, OUTPUT_OMEGA_MIN, OUTPUT_OMEGA_MAX));
     return;
 }
