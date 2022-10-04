dea3732416c3933a3647e121e8af2574c71eae6c
DavidFreire-FEUP <up201806711@fe.up.pt>
2022-10-04 20:49:42 +0100

    fix mistake in joystick position method

--- arduino/main/Joystick.cpp
+++ arduino/main/Joystick.cpp
@@ -47,7 +47,7 @@ LinearCoords Joystick::position()
 {
     LinearCoords pos;
     pos.x = this->pinInterface->doAnalogRead(this->xPin);
-    pos.y = this->pinInterface->doAnalogRead(this->xPin);
+    pos.y = this->pinInterface->doAnalogRead(this->yPin);
     pos = joystickMapping(pos);
     return pos;
 }
