6a50548a96c061df18e7a2a27cb5f4effc24f6ba
Francisco Samagaio <78874837+FranciscoSamagaio@users.noreply.github.com>
2022-10-06 17:34:31 +0100

    converts XY to Velocity Omega

--- arduino/main/Coordinates.cpp
+++ arduino/main/Coordinates.cpp
@@ -1,11 +1,13 @@
 // TODO: #1 Create function that converts the joystick XY coordinates to Velocity Omega wheelchair input
 
 #include "Coordinates.h"
+#include <math.h>
 
 PolarCoords Coordinates::linearToPolar(LinearCoords coords)
 {
-    // magic ...
-    PolarCoords polarCoords = {.v = 0.0, .omega = 0.0};
+    PolarCoords polarCoords = {.v = 0.0 , .omega = 0.0};
+    polarCoords.v = sqrt(pow(coords.x,2) + pow(coords.y,2)); 
+    polarCoords.omega = atan(coords.y / coords.x) * 180/(2*asin(1.0));
     return polarCoords;
 }
 
