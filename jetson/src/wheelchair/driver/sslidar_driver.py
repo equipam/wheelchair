6b890035c40d106cad6c635c6550f0f743b71ec1
DavidFreire-FEUP <up201806711@fe.up.pt>
2022-10-07 15:06:54 +0100

    separate folder for driver, add libs

--- jetson/src/wheelchair/driver/sslidar_driver.py
+++ jetson/src/wheelchair/driver/sslidar_driver.py
@@ -1,5 +1,9 @@
 #TODO #15 Create SSLidar usb driver
 
+import pathlib
+
+_FOLDER_PATH = pathlib.Path(__file__).parent.resolve()
+
 class SSLidarDriver:
     def __init__(self) -> None:
         pass
