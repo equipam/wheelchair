048c8f478dee1dad37e1a21837342584c7a59edf
DavidFreire-FEUP <up201806711@fe.up.pt>
2022-10-14 17:10:57 +0100

    test code, ignore

--- jetson/src/wheelchair/arduino_test.py
+++ jetson/src/wheelchair/arduino_test.py
@@ -6,6 +6,9 @@ from wheelchair.arduino import Arduino
 arduino = Arduino(18)
 
 while True:
+    print("Rise")
     arduino.set_stop_signal(True)
     sleep(5)
+    print("Fall")
     arduino.set_stop_signal(False)
+    sleep(5)
