7f8c014699b37cbb7da0b4adf4cdfc017f58bd54
DavidFreire-FEUP <up201806711@fe.up.pt>
2022-10-14 16:35:14 +0100

    arduino jetson nano test

--- jetson/src/wheelchair/arduino_test.py
+++ jetson/src/wheelchair/arduino_test.py
@@ -1,1 +1,11 @@
 #TODO
+
+from time import sleep
+from wheelchair.arduino import Arduino
+
+arduino = Arduino(18)
+
+while True:
+    arduino.set_stop_signal(True)
+    sleep(5)
+    arduino.set_stop_signal(False)
