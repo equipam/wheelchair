30d817dd1b102958fba841ccd9ddb00f07f1a47c
DavidFreire-FEUP <up201806711@fe.up.pt>
2022-10-16 19:47:19 +0100

    fix printf

--- jetson/lib/main.cpp
+++ jetson/lib/main.cpp
@@ -113,13 +113,13 @@ static void eventCallbackFunction(int handle, int eventType, uint8_t *data, int 
 	{
 	// Measurement data notification events
 	case HPS3D_SIMPLE_ROI_EVEN:
-		print("HPS3D_SIMPLE_ROI_EVEN\n");
+		printf("HPS3D_SIMPLE_ROI_EVEN\n");
 		break;
 	case HPS3D_FULL_ROI_EVEN:
-		print("HPS3D_FULL_ROI_EVEN\n");
+		printf("HPS3D_FULL_ROI_EVEN\n");
 		break;
 	case HPS3D_FULL_DEPTH_EVEN:
-		print("HPS3D_FULL_DEPTH_EVEN\n");
+		printf("HPS3D_FULL_DEPTH_EVEN\n");
 		break;
 	case HPS3D_SIMPLE_DEPTH_EVEN:
 		printf("handle:%d!\n", handle);
@@ -137,10 +137,10 @@ static void eventCallbackFunction(int handle, int eventType, uint8_t *data, int 
 		}
 		break;
 	case HPS3D_NULL_EVEN:
-		print("HPS3D_NULL_EVEN\n");
+		printf("HPS3D_NULL_EVEN\n");
 		break;
 	default:
-		print("Not known event\n");
+		printf("Not known event\n");
 		break;
 	}
 }
