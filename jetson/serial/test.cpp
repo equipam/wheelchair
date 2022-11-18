2d4abd935ce9c24ee3a01d69b0e6a4b24808227f
up201806652 <78861546+up201806652@users.noreply.github.com>
2022-11-18 15:52:07 +0000

    Test version 1
    Need to verify if Arduino receives the data

---
+++ jetson/serial/test.cpp
@@ -0,0 +1,14 @@
+//Compile with -lserial and run with sudo 
+#include <libserial/SerialPort.h>
+
+using namespace LibSerial; 
+
+int main(){
+	SerialStream serial;
+	serial.Open("/dev/ttyACM0");
+	serial.SetBaudRate( BAUD_115200 );
+	std::string aux = "Teste";
+	serial << aux;
+	
+	serial.Close();
+}
