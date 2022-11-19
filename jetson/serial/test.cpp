90469f833392db10d314ba915db2971871f7a9dc
up201806652 <78861546+up201806652@users.noreply.github.com>
2022-11-19 14:55:58 +0000

    Baudrate change

--- jetson/serial/test.cpp
+++ jetson/serial/test.cpp
@@ -6,7 +6,7 @@ using namespace LibSerial;
 int main(){
 	SerialStream serial;
 	serial.Open("/dev/ttyACM0");
-	serial.SetBaudRate( BAUD_115200 );
+	serial.SetBaudRate( BaudRate::BAUD_115200 );
 	std::string aux = "Teste";
 	serial << aux;
 	
