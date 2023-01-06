d36ac8d21d5d1ac361b52ec011445f760698b9ee
DavidFreire-FEUP <up201806711@fe.up.pt>
2023-01-06 16:34:44 +0000

    testing serial interface

---
+++ jetson/serial_testing/main.cpp
@@ -0,0 +1,27 @@
+#include "../lib/SerialInterface.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    SerialInterface serial();
+    char * port = "/dev/ttyUSB0";
+    serial.setup(port);
+    int overwrite = 0;
+    int counter = 0;
+    std::cout << overwrite << std::endl;
+    while (true)
+    {
+        serial.sendPacket(overwrite, 0f, 0f);
+        counter++;
+        if(counter == 1000)
+        {
+            counter = 0;
+            overwrite = (overwrite + 1) % 3;
+            std::cout << overwrite << std::endl;
+        }
+    }
+    
+    return 0;
+}
